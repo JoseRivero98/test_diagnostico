d2e300d83cccc0b2592b900af0305e2f853aafa4
José Rivero <jose.rivero7916@alumnos.udg.mx>
2020-09-20 06:35:45 -0500

    Mostrar una cadena n veces

--- main.c
+++ main.c
@@ -22,10 +22,19 @@ void capturar_enteros(){
     printf("Promedio: %f\n", suma/5.0);
 }
 
+void mostrar(int n, char cadena[]){
+    for (size_t i =0; i < n; i++)
+    {
+        printf("%s", cadena);
+    }
+}
+
 
 int main()
 {
     char op;
+    char cadena[20];
+    int n;
 
     do
     {
@@ -42,6 +51,12 @@ int main()
             capturar_enteros();
             break;
         case '2':
+             printf("Escribe una cadena de hasta 20 caracteres: ");
+            fflush(stdin);
+            fgets(cadena, sizeof(cadena), stdin);
+            printf("n: ");
+            scanf("%i", &n);
+            mostrar(n, cadena);
             break;
         case '3':
             break;
